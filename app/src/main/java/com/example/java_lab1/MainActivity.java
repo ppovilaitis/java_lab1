9554d6753cad551ca7b992b23941d808a0f52dfc
ppovilaitis <povilaitispranas@gmail.com>
2022-09-24 19:54:34 +0300

    Added button to change TextView text

--- app/src/main/java/com/example/java_lab1/MainActivity.java
+++ app/src/main/java/com/example/java_lab1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.java_lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMainBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMainBox = findViewById(R.id.tvMainBox);
+    }
+
+    public void onBtnClickTextChange(View view) {
+        this.tvMainBox.setText("Text successfully changed");
     }
 }
